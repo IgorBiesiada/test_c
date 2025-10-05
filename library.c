12c847db549b7b16d711c4a17db857ff9e1b2a5d
IgorBiesiada <xxxigorbiesiada1@gmail.com>
2025-10-05 17:32:23 +0200

    01 zmiana metody policz

--- library.c
+++ library.c
@@ -2,6 +2,11 @@
 
 #include <stdio.h>
 
-void hello(void) {
+ibnt Policz(int a, int b) {
+    return a + b;
+}
+
+void main(void) {
     printf("Hello, World!\n");
+    printf("W pierwszym branchu 2+3", Policz(2, 3));
 }
